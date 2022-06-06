dc3ad6a1b68926a87b9d579d7a06a3604858e7f6
SatwikGouda <84791468+SatwikGouda@users.noreply.github.com>
2022-06-06 17:20:27 +0530

    Solution-2

--- setMatrixZero1.cpp
+++ setMatrixZero1.cpp
@@ -33,3 +33,37 @@ void setZeros(vector<vector<int>> &matrix)
         }
     }
 }
+
+//-------------------------------------------------------------
+
+//Solution - 2
+// Checking which cols and rows contain 0 with help of dummy arrays
+//and then updating it to 0 accordingly.
+
+#include <bits/stdc++.h> 
+void setZeros(vector<vector<int>> &matrix)
+{
+    // Write your code here.
+    int row=matrix.size();
+    int col=matrix[0].size();
+    vector<int> tempRows(row,-1);
+    vector<int> tempCols(col,-1);
+    
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(matrix[i][j] == 0){
+                tempRows[i] = 0;
+                tempCols[j] = 0;
+            }
+        }
+    }
+    
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(tempRows[i] == 0 || tempCols[j] ==0){
+                matrix[i][j] =0;
+            }
+        }
+    }
+}
+
