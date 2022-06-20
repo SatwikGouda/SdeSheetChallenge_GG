58ce72944c617f52ed8af5ec7d5246232bb631a2
SatwikGouda <84791468+SatwikGouda@users.noreply.github.com>
2022-06-20 10:25:16 +0530

    Longest Common Prefix -2

--- longestCommonPrefix.cpp
+++ longestCommonPrefix.cpp
@@ -24,3 +24,41 @@ string longestCommonPrefix(vector<string> &arr, int n)
 }
 
 
+// Approach - 2
+string prefixAns(string &s1,string &s2){
+    int minLength = min(s1.size(),s2.size());
+    // Only traversing through minimum length of either s1 ,s2    
+    string ans = "";
+    for(int i=0;i<minLength;i++){
+        if(s1[i] == s2[i]){
+            ans += s1[i];
+        }
+        else{
+            break;
+        }
+    }
+    return ans;
+}
+
+string divideString(vector<string> &arr,int start,int end){
+    if(start == end){
+        return arr[start];    //edge case
+    }
+    //Divide nd Conquer Approach    
+    int mid = start + (end-start)/2;
+    string leftHalf = divideString(arr,start,mid);
+    string rightHalf = divideString(arr,mid+1,end);
+    
+    string ans = prefixAns(leftHalf , rightHalf);
+    return ans;
+    
+}
+
+
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    string longestPrefix = divideString(arr,0,n-1);
+    return longestPrefix;
+}
+
+
